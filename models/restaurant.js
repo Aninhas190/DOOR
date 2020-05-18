4b1643d391342879d693fdbcf09a193237856c41
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-18 14:41:40 +0100

    first draft restaurant model

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,1 +1,54 @@
-'use strict'
+'use strict';
+
+const mongoose = require('mongoose');
+
+const RestaurantSchema = new mongoose.Schema({
+  name: String,
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [
+      {
+        type: Number,
+        min: -180,
+        max: 180
+      }
+    ]
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  description: {
+    type: String
+  },
+  images: {
+    type: String
+  },
+  cuisineType: String,
+  contact: {
+    type: Number
+  },
+  averagePrice: {
+    type: Number
+  },
+  menu: {
+    dishName: {
+      type: String
+    },
+    allergies: [{
+      type: String,
+      enum: ['gluten', 'celery', 'sesame', 'nuts', 'lupin', 'peanuts', 'fish', 'milk', 'mollusks', 'sulphites', 'crustaceans', 'wheat', 'mustard']
+    }],
+    dishDescription: String
+  }
+});
+
+RestaurantSchema.index({ location: '2dshpere' });
+
+const Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+
+module.exports = Restaurant;
