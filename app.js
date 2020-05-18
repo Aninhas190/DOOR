357c7d234f0cf878f39ceb64318eacc41cd377b8
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-18 12:33:54 +0100

    route update

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ const authenticationRouter = require('./routes/authentication');
 
 const app = express();
 
-console.log('hello');
-
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
