f564797a4fe41b520fd7f603f8123f484ddc9aed
nanori <leonor.ferreira@gmail.com>
2020-05-19 08:31:41 +0100

    Monday last push

--- routes/user.js
+++ routes/user.js
@@ -12,19 +12,37 @@ userRouter.get('/', (req, res, next) => {
 });
 
 // Route to create allergy list
-userRouter.get('/edit', (req, res, next) => {
-  res.render('foodie/edit');
+userRouter.get('/:userId/edit', (req, res, next) => {
+  const userId = req.params.userId;
+  const name = req.body.name;
+  const type = req.body.type;
+
+  Place.findById(userId).then((place) => {
+    const name = req.params.name;
+    const type = req.params.type;
+    res.render('place/edit', { place });
+  });
 });
 
-userRouter.post('/edit', (req, res, next) => {
-  const allergies = req.body.allergies;
-  User.updateOne({ allergies })
-    .then((user) => {
-      res.redirect('/');
-    })
-    .catch((error) => next(error));
+userRouter.post('/:userId/edit', (req, res, next) => {
+  const userId = req.params.userId;
+  Place.findById(userId).then((user) => {
+    res.render('place/edit', { place });
+  });
 });
 
+// userRouter.get('/edit', (req, res, next) => {
+//   res.render('foodie/edit');
+//   // res.render('foodie/edit', { checkedCelery: true });
+// });
 
+// userRouter.post('/edit', (req, res, next) => {
+//   const allergies = req.body.allergies;
+//   User.updateOne({ allergies })
+//     .then((user) => {
+//       res.redirect('/');
+//     })
+//     .catch((error) => next(error));
+// });
 
 module.exports = userRouter;
