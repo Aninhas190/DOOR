96b2134a78a2e1f8de0e698378544304ca43480a
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-19 11:40:15 +0100

    route restaurant/create done

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,9 +2,12 @@
 
 const { Router } = require('express');
 const restaurantRouter = new Router();
-const ZOMATO_API_KEY = process.env.ZOMATO_API_KEY;
 
 const Restaurant = require('./../models/restaurant');
+
+const routeGuard = require('./../middleware/route-guard')
+
+const ZOMATO_API_KEY = process.env.ZOMATO_API_KEY;
 const Zomato = require('zomato.js');
 const zomato = new Zomato(ZOMATO_API_KEY);
 
@@ -17,16 +20,34 @@ restaurantRouter.get('/createByZomatoId', (req, res, next) => {
 }); 
 
 restaurantRouter.get('/createByZomatoId', (req, res, next) => {
-  res.render('restaurant/create');
+  //const restaurantId = req.body.zomatoId;
+  console.log(req.body);
+  /*zomato.restaurant({res_id: restaurantId})
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => next(error));*/
+  res.redirect('/');
 }); 
 
 //manually
-restaurantRouter.get('/create', (req, res, next) => {
+restaurantRouter.get('/create', routeGuard, (req, res, next) => {
   res.render('restaurant/create');
 }); 
 
 restaurantRouter.post('/create', (req, res, next) => {
-  const { name, description, image, latitute, longitute, cuisineType, contact} = req.body;
+  const ownerId = req.user;
+  const { name, description, latitute, longitute, cuisineType, contact} = req.body;
+  Restaurant.create({
+    name,
+    description,
+    latitute,
+    cuisineType,
+    contact,
+    owner: ownerId
+  })
+    .then(restaurant => res.render('restaurant/restaurant', {restaurant}))
+    .catch(error=> next(error));
 });
 
 restaurantRouter.get('/list', (req, res, next) => {
