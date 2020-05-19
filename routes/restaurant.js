4d6b9347c046446207f42d8946bc785729647926
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-19 10:59:45 +0100

    zomato try out

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,19 +2,36 @@
 
 const { Router } = require('express');
 const restaurantRouter = new Router();
+const ZOMATO_API_KEY = process.env.ZOMATO_API_KEY;
 
 const Restaurant = require('./../models/restaurant');
+const Zomato = require('zomato.js');
+const zomato = new Zomato(ZOMATO_API_KEY);
 
 restaurantRouter.get('/', (req, res, next) => {
   res.render('restaurant/restaurant');
 });
+//by zomato ID
+restaurantRouter.get('/createByZomatoId', (req, res, next) => {
+  res.render('restaurant/createByZomatoId');
+}); 
 
+restaurantRouter.get('/createByZomatoId', (req, res, next) => {
+  res.render('restaurant/create');
+}); 
+
+//manually
 restaurantRouter.get('/create', (req, res, next) => {
   res.render('restaurant/create');
 }); 
 
-// restaurantRouter.post('/create', (req, res, next) => {
-//   const { name, description, }
-// })
+restaurantRouter.post('/create', (req, res, next) => {
+  const { name, description, image, latitute, longitute, cuisineType, contact} = req.body;
+});
+
+restaurantRouter.get('/list', (req, res, next) => {
+  res.render('restaurant/list');
+});
+
 
 module.exports = restaurantRouter;
