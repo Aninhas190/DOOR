4dd83bbe3be010740a8943e544abf85476e02175
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-21 10:17:58 +0100

    update profile route

--- routes/authentication.js
+++ routes/authentication.js
@@ -103,24 +103,6 @@ authenticationRouter.post('/log-in', (req, res, next) => {
     });
 });
 
-authenticationRouter.get('/profile', routeGuard, (req, res, next) => {
-  let user;
-  const userType = req.user.userType;
-
-  User.findById(req.user._id)
-    .then((document) => {
-      user = document.toObject();
-      if (userType === 'restaurantOwner') {
-        user.isOwner = true;
-      }
-      console.log('user', user);
-      console.log('document', document);
-      res.render('profile', { document, user });
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
 
 authenticationRouter.post('/sign-out', (req, res) => {
   req.session.destroy();
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,29 @@
 const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
+const User = require('./../models/user');
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'DOOR' });
 });
 
 router.get('/profile', routeGuard, (req, res, next) => {
-  res.render('profile');
+  let user;
+  const userType = req.user.userType;
+
+  User.findById(req.user._id)
+    .then((document) => {
+      user = document.toObject();
+      if (userType === 'restaurantOwner') {
+        user.isOwner = true;
+      }
+      console.log('user', user);
+      console.log('document', document);
+      res.render('profile', { document, user });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
